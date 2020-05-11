7a2843d048f49ec16008f1182a96f4c82da0a12c
Vikas Rathour <45872852+X3Vikas@users.noreply.github.com>
2020-05-11 14:08:38 +0530

    initial commit

---
+++ assets/js/init.js
@@ -0,0 +1,8 @@
+
+// $(document).ready(function () {
+//   $('.sidenav').sidenav();
+//   $('.parallax').parallax();
+//   $('.dropdown-trigger').dropdown();
+//   $('.dropdown-trigger1').dropdown();
+// });
+
